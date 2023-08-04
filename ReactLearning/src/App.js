e686c008f028cd35e03f82db693393bdd7e3791e
fengzi <892726098@qq.com>
2023-08-04 17:51:24 +0800

    就测试一下github还能不能用

--- ReactLearning/src/App.js
+++ ReactLearning/src/App.js
@@ -7,7 +7,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='box'>
-        <User name='可莉' age='12' gender={'女'} /> {/* name和gender传的方法没差，都是传字符串 */}
+        <User name='可莉' age='12' gender={'女'} /> {/* name和gender传的方法没差，都是传字符串呢 */}
       </div>
     )
   }
