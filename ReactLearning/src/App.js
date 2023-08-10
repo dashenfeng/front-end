bc050d769dbfcc253b94aebbe7f50b3e8afc3eaf
fengzi <892726098@qq.com>
2023-08-10 23:16:20 +0800

    8.10

--- ReactLearning/src/App.js
+++ ReactLearning/src/App.js
@@ -1,13 +1,55 @@
 import Logs from "./Components/Logs/Logs";
-import LogsForm from "./Components/LogsForm/LogsForm"
-import "./Components/LogsForm/LogsForm.css"
-import "./App.css"
+import LogsForm from "./Components/LogsForm/LogsForm";
+import "./Components/LogsForm/LogsForm.css";
+import "./App.css";
+import { useState } from "react";
 
 const App = () => {
-  return <div className="app">
-    <LogsForm></LogsForm>
-    <Logs/>
-  </div>
+  // 模拟一组从服务器中加载的数据
+  const [logsData, setLogsData] = useState([
+    {
+      id: "001",
+      date: new Date(2021, 1, 20, 18, 30),
+      desc: "好好搬砖",
+      time: 30,
+    },
+    {
+      id: "002",
+      date: new Date(2022, 2, 10, 12, 30),
+      desc: "好好学习React",
+      time: 20,
+    },
+    {
+      id: "003",
+      date: new Date(2022, 2, 11, 11, 30),
+      desc: "坚持锻炼",
+      time: 40,
+    },
+    {
+      id: "004",
+      date: new Date(2022, 2, 15, 10, 30),
+      desc: "想万万",
+      time: 80,
+    },
+  ])
+  /* 
+    1.将logsData传给Logs,使其可以正常显示数据 -->「父传子」
+    2.将LogsForm中，『用户提交的数据』传递（函数传参方法）给APP组件，将新的Logs添加到上面数组中-->「子传父」
+  */
+ const saveLogHandler = (newLog) => {
+    console.log(newLog);
+    newLog.id = Date.now() + '' // 生成唯一id(其实用时间戳会有并发风险，有极小的几率不唯一)
+    setLogsData([
+      ...logsData,
+      newLog
+    ])
+ }
+  return (
+    <div className="app">
+      <LogsForm onSaveLog={saveLogHandler}></LogsForm>
+      <Logs logsData={logsData} />
+    </div>
+  );
 };
 
 // 导出App
--- ReactLearning/src/Components/LogsForm/LogsForm.js
+++ ReactLearning/src/Components/LogsForm/LogsForm.js
@@ -1,36 +1,43 @@
 import React, { useState } from "react";
 import Card from "../UI/Card/Card";
 
-export default function LogsForm() {
+export default function LogsForm(props) {
+  //   // 将表单数据统一到一个state对象中
+  // const [formData, setFormData] = useState({
+  //   date:'',
+  //   desc:'',
+  //   time:'',
+  // })
   /* 这边用useState，表单里input添加value，实现双向绑定 */
-  const [date,setDate] = useState('')
-  const [desc,setDesc] = useState('')
-  const [time,setTime] = useState('')
+  const [date, setDate] = useState("");
+  const [desc, setDesc] = useState("");
+  const [time, setTime] = useState("");
 
   const dateChangeHandler = (e) => {
     // console.log("date is changed", e.target.value);
-    setDate(e.target.value)
+    setDate(e.target.value);
   };
   const descChangeHandler = (e) => {
     // console.log("desc is changed", e.target.value);
-    setDesc(e.target.value)
+    setDesc(e.target.value);
   };
   const timeChangeHandler = (e) => {
     // console.log("time is changed", e.target.value);
-    setTime(e.target.value)
+    setTime(e.target.value);
   };
   const formSubmit = (e) => {
     e.preventDefault(); // 取消表单的默认行为(跳转)
     const newLog = {
-        date:new Date(date),
-        desc,
-        time
-    }
-    console.log(newLog);
-    setDate('')
-    setDesc('')
-    setTime('')
+      date: new Date(date),
+      desc,
+      time,
+    };
+    // console.log(newLog);
+    props.onSaveLog(newLog)
 
+    setDate("");
+    setDesc("");
+    setTime("");
   };
 
   return (
@@ -38,15 +45,27 @@ export default function LogsForm() {
       <form onSubmit={formSubmit}>
         <div className="form-item">
           <label htmlFor="date">日期</label>
-          <input id="date" type="date" onChange={dateChangeHandler} value={date}></input>
+          <input
+            id="date"
+            type="date"
+            onChange={dateChangeHandler}
+            value={date}></input>
         </div>
         <div className="form-item">
           <label htmlFor="desc">内容</label>
-          <input id="desc" type="text" onChange={descChangeHandler} value={desc}></input>
+          <input
+            id="desc"
+            type="text"
+            onChange={descChangeHandler}
+            value={desc}></input>
         </div>
         <div className="form-item">
           <label htmlFor="time">时长</label>
-          <input id="time" type="number" onChange={timeChangeHandler} value={time}></input>
+          <input
+            id="time"
+            type="number"
+            onChange={timeChangeHandler}
+            value={time}></input>
         </div>
         <div className="form-btn">
           <button>添加</button>
