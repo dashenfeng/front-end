3c5102374461d5875f8e13b19e9b945fb700f269
fengzi <892726098@qq.com>
2023-08-19 09:28:42 +0800

    8.18

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+    return (
+        <div>
+            <p className='pstyle'>Hello React!</p>
+            <button>123</button>
+           
+        </div>
+    );
+};
+
+export default App;
