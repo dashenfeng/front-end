caf956ee3045e245711134a649778b4d28a45481
fengzi <892726098@qq.com>
2023-10-29 09:25:09 +0800

    10.27

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,48 +1,51 @@
 import React from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {setName, setAge} from './store/stuSlice';
-import {setName as setSchoolName, setAddress as setSchoolAddress} from "./store/schoolSlice";
+import {useDelStudentMutation, useGetStudentsQuery} from "./store/studentApi";
+import StudentList from "./components/StudentList";
+
 
 const App = () => {
-    // useSelector() 用来加载state中的数据
-    // const student = useSelector(state => state.student);
-    // // 引入学校的state
-    // const school = useSelector(state => state.school);
-    const {student, school} = useSelector(state => state);
+
+    const result = useGetStudentsQuery(null, {
+        // useQuery可以接收一个对象作为第二个参数，通过该对象可以对请求进行配置
+        // selectFromResult: result => {
+        //     if (result.data) {
+        //         result.data = result.data.filter(item => item.attributes.age < 18);
+        //     }
+        //
+        //     return result;
+        // }, // 用来指定useQuery返回的结果
+
+        pollingInterval:0, // 设置轮询的间隔，单位毫秒 如果为0则表示不轮询
+        skip:false, // 设置是否跳过当前请求，默认false
+        refetchOnMountOrArgChange:false, // 设置是否每次都重新加载数据 false正常使用缓存，
+                                            // true每次都重载数据
+                                            //数字，数据缓存的时间（秒）
+        refetchOnFocus:false, // 是否在重新获取焦点时重载数据
+        refetchOnReconnect:true, // 是否在重新连接后重载数据
+    });
+
+    /*
+        currentData: undefined // 当前参数的最新数据
+        data: undefined // 最新的数据
+        isError: false // 布尔值，是否有错误
+        error: Error() // 对象，有错时才存在
+        isFetching: true // 布尔值，数据是否在加载
+        isLoading: true // 布尔值，数据是否第一次加载
+        isSuccess: false // 布尔值，请求是否成功
+        isUninitialized: false // 布尔值，请求是否还没有开始发送
+        refetch: ƒ () // 一个函数，用来重新加载数据
+        status: "pending" // 字符串，请求的状态
+    * */
 
 
-    // 通过useDispatch()来获取派发器对象
-    const dispatch = useDispatch();
-    // 获取action的构建器
+    const {data: stus, isSuccess, isLoading, refetch} = result; // 调用Api中的钩子查询数据
 
-
-    const setNameHandler = () => {
-        dispatch(setName('沙和尚'));
-    };
-
-    const setAgeHandler = () => {
-        dispatch(setAge(33));
-    };
 
     return (
         <div>
-            <p>
-                {student.name} ---
-                {student.age} ---
-                {student.gender} ---
-                {student.address}
-            </p>
-            <button onClick={setNameHandler}>修改name</button>
-            <button onClick={setAgeHandler}>修改age</button>
-
-            <hr/>
-
-            <p>
-                {school.name} ---
-                {school.address}
-            </p>
-            <button onClick={()=>dispatch(setSchoolName('高老庄中小'))}>修改学校名字</button>
-            <button onClick={()=>dispatch(setSchoolAddress('高老庄府前街19号'))}>修改学校地址</button>
+            <button onClick={() => refetch()}>刷新</button>
+            {isLoading && <p>数据加载中...</p>}
+            {isSuccess && <StudentList stus={stus}/>}
         </div>
     );
 };
--- react-app/src_RTKQ/store/index.js
+++ react-app/src_RTKQ/store/index.js
@@ -1,6 +1,5 @@
 import {configureStore} from "@reduxjs/toolkit";
 import studentApi from "./studentApi";
-import {setupListeners} from "@reduxjs/toolkit/query";
 
 const store = configureStore({
     reducer:{
@@ -10,7 +9,5 @@ const store = configureStore({
     middleware:getDefaultMiddleware =>
         getDefaultMiddleware().concat(studentApi.middleware)
 });
-
-setupListeners(store.dispatch); // 设置以后，将会支持 refetchOnFocus refetchOnReconnect
 
 export default store;
